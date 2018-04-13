4fc6a8353a7549c1bc84dcb46073383383bb6511
Sigfrido Gómez <sigfridgom@gmail.com>
2018-04-12 23:23:09 -0600

    ....

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+  <html>
+  <head>
+<meta charset= "utf-8">
+<title>BIENVENIDO A WWW.NUEGADOS.COM </title>
+</head>
+  <body>
+<h1>¡ Lo lograste! El virtual host www.nuegados.com está funcionando !</h1>
+
+ <?php
+print "El hostname del servidor es: ";
+echo gethostname();
+?>
+  </body>
+</html>
